0559b401d56ea45045fe6f4bd2816d551f072c12
Huvert0101 <89505380+Huvert0101@users.noreply.github.com>
2022-04-03 01:05:30 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+//settings
+app.set('port', process.env.PORT || 3000);
+//static files;
+app.use(express.static(path.join(__dirname, '/public')));
+//start the server
+const server = app.listen(app.get('port'), ()=> {
+    console.log('Server on port', app.get('port'));
+});
+const SocketIO = require('socket.io');
+const io = SocketIO(server);
+//websockets
+io.on('connection', (socket) => {
+    console.log('New connection', socket.id);
+    socket.on('chat:message', (data) => {
+        io.sockets.emit('chat:message', data);
+    });
+    socket.on('chat:typing', (data) => {
+        socket.broadcast.emit('chat:typing', data);
+    });
+});
